61cb8fa854763cbb142e9a3aa96041531f0caf02
Josh Valentino <53629621+valentinojosh@users.noreply.github.com>
2025-06-29 00:56:15 -0500

    Update Foo.java

--- Code/Foo.java
+++ Code/Foo.java
@@ -37,19 +37,14 @@ public class Foo {
         Paging<PlaylistTrack> playlistTrackPaging = getSpotifyData();
 
         //Method that checks if the absolute path to the local repo is present and deletes it if so
-        //This is because when cloning the repository it will throw an error if it is already present locally
         checkLocalRepo();
 
-        // Method to Clone the GitHub repository to a local repo
         cloneRemoteRepo();
 
-        // Method to update the file and write it passing the spotify data
         writeUpdatedFile(playlistTrackPaging);
 
-        // Method to commit the changes to the GitHub repo
         commitRemoteRepo();
 
-        // Method to push the committed changes to GitHub Repo
         pushRemoteRepo();
     }
 
